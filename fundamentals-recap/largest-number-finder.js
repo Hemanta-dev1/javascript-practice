8a6d045bf14d78a375c4e06ef00fa7ac956db629
Hemanta-dev1 <iamhemanta14@gmail.com>
2026-07-16 22:33:54 +0545

    largest-number-finder

---
+++ fundamentals-recap/largest-number-finder.js
@@ -0,0 +1,25 @@
+function largestOfAll(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let largest = arr[i][0];
+
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largest) {
+        largest = arr[i][j];
+      }
+    }
+
+    result.push(largest);
+  }
+
+  return result;
+}
+console.log(
+  largestOfAll([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+  ])
+);
