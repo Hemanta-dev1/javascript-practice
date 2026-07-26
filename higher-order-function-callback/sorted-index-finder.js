215a4ad3ea07264e041162f79ad781910af48212
Hemanta-dev1 <iamhemanta14@gmail.com>
2026-07-26 19:52:06 +0545

    writing-tests

--- higher-order-function-callback/sorted-index-finder.js
+++ higher-order-function-callback/sorted-index-finder.js
@@ -6,3 +6,6 @@ function getIndexToIns(arr, num) {
   }
   return index;
 }
+console.log(getIndexToIns([1, 2, 3, 4], 1.5));
+console.log(getIndexToIns([20, 3, 5], 19));
+console.log(getIndexToIns([3, 4, 5], 2));
