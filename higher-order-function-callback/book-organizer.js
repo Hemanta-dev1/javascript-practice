54e240895a49ba285204a6ac1f5bbc894fb7d569
Hemanta-dev1 <iamhemanta14@gmail.com>
2026-07-26 19:05:05 +0545

    created list of objects for books array

---
+++ higher-order-function-callback/book-organizer.js
@@ -0,0 +1,17 @@
+let books = [
+  {
+    title: "harry poter",
+    authorName: "hemanta",
+    releaseYear: 1945,
+  },
+  {
+    title: "GOT",
+    authorName: "Mike",
+    releaseYear: 2019,
+  },
+  {
+    title: "Breaking Bad",
+    authorName: "Haisenberg",
+    releaseYear: 1938,
+  },
+];
