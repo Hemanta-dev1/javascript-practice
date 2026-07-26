2ab8108cdee64dc50446401f98154c564c7a44de
Hemanta-dev1 <iamhemanta14@gmail.com>
2026-07-26 19:07:57 +0545

    completed-book-organizer

--- higher-order-function-callback/book-organizer.js
+++ higher-order-function-callback/book-organizer.js
@@ -15,3 +15,19 @@ let books = [
     releaseYear: 1938,
   },
 ];
+
+function sortByYear(book1, book2) {
+  if (book1.releaseYear < book2.releaseYear) {
+    return -1;
+  }
+  if (book1.releaseYear > book2.releaseYear) {
+    return 1;
+  }
+  if (book1.releaseYear === book2.releaseYear) {
+    return 0;
+  }
+}
+
+let filteredBooks = books.filter((book) => book.releaseYear <= 1950);
+filteredBooks.sort(sortByYear);
+console.log(filteredBooks);
