9c21025c8f4e4738f5ceba750332aff70bec5cab
Hemanta-dev1 <iamhemanta14@gmail.com>
2026-07-30 21:26:38 +0545

    flattening-tool

---
+++ higher-order-function-callback/flattening-tool.js
@@ -0,0 +1,13 @@
+function steamrollArray(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(steamrollArray(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
